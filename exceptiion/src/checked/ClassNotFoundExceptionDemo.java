f396d4f72f24bfd221817e898758cb194a05e579
danu <danujagreru@gmail.com>
2023-09-12 13:27:15 +0530

    ClassNotFoundException

---
+++ exceptiion/src/checked/ClassNotFoundExceptionDemo.java
@@ -0,0 +1,16 @@
+package checked;
+
+/*
+    @author DanujaV
+    @created 9/12/23 - 1:23 PM   
+*/
+
+public class ClassNotFoundExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            Class.forName("Studennt.class"); //Student load
+        } catch (ClassNotFoundException e) {
+            System.out.println("class not found!");
+        }
+    }
+}
